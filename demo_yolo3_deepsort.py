b1dc22bfe9069e13dd92d183d4f54fd8ff369def
hyperfraise <damien.menigaux@polytechnique.edu>
2019-04-16 15:08:15 +0200

    batchification

--- demo_yolo3_deepsort.py
+++ demo_yolo3_deepsort.py
@@ -12,6 +12,7 @@ import time
 class Detector(object):
     def __init__(self):
         self.vdo = cv2.VideoCapture()
+        self.batch_size = 36
         self.yolo3 = YOLO3(
             "YOLO3/cfg/yolo_v3.cfg",
             "YOLO3/yolov3.weights",
@@ -37,34 +38,42 @@ class Detector(object):
 
     def detect(self):
         xmin, ymin, xmax, ymax = self.area
+        ims, ori_ims = [], []
+        start = time.time()
         while self.vdo.grab():
-            for i in range(9):
-                self.vdo.grab()
-            start = time.time()
             _, ori_im = self.vdo.retrieve()
-            im = ori_im[ymin:ymax, xmin:xmax, (2, 1, 0)]
-            bbox_xywh, cls_conf, cls_ids = self.yolo3(im)
-            if bbox_xywh is not None:
-                mask = cls_ids == 0
-                bbox_xywh = bbox_xywh[mask]
-                bbox_xywh[:, 3] *= 1.2
-                cls_conf = cls_conf[mask]
-                outputs = self.deepsort.update(bbox_xywh, cls_conf, im)
-                if len(outputs) > 0:
-                    bbox_xyxy = outputs[:, :4]
-                    identities = outputs[:, -1]
-                    ori_im = draw_bboxes(
-                        ori_im, bbox_xyxy, identities, offset=(xmin, ymin)
+            ori_ims.append(ori_im)
+            ims.append(ori_im[ymin:ymax, xmin:xmax, (2, 1, 0)])
+            if len(ims) == self.batch_size:
+                batch_bbox_xywh, batch_cls_conf, batch_cls_ids = self.yolo3(ims)
+                assert len(batch_bbox_xywh) == self.batch_size
+                for i, (bbox_xywh, cls_conf, cls_ids) in enumerate(
+                    zip(batch_bbox_xywh, batch_cls_conf, batch_cls_ids)
+                ):
+
+                    if bbox_xywh is not None:
+                        mask = cls_ids == 0
+                        bbox_xywh = bbox_xywh[mask]
+                        bbox_xywh[:, 3] *= 1.2
+                        cls_conf = cls_conf[mask]
+                        outputs = self.deepsort.update(bbox_xywh, cls_conf, ims[i])
+                        if len(outputs) > 0:
+                            bbox_xyxy = outputs[:, :4]
+                            identities = outputs[:, -1]
+                            ori_ims[i] = draw_bboxes(
+                                ori_ims[i], bbox_xyxy, identities, offset=(xmin, ymin)
+                            )
+                    if self.write_video:
+                        self.output.write(ori_ims[i])
+                ims, ori_ims = [], []
+
+                end = time.time()
+                print(
+                    "time: {}s, fps: {}".format(
+                        end - start, self.batch_size / (end - start)
                     )
-
-            end = time.time()
-            print("time: {}s, fps: {}".format(end - start, 1 / (end - start)))
-
-            # cv2.imshow("test", ori_im)
-            # cv2.waitKey(1)
-
-            if self.write_video:
-                self.output.write(ori_im)
+                )
+                start = time.time()
 
 
 if __name__ == "__main__":
@@ -73,8 +82,6 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Usage: python demo_yolo3_deepsort.py [YOUR_VIDEO_PATH]")
     else:
-        cv2.namedWindow("test", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("test", 800, 600)
         det = Detector()
         det.open(sys.argv[1])
         det.detect()
